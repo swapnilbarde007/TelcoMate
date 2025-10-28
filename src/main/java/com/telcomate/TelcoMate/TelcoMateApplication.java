3a2f71fbb773439baea5628ee1bb22f9161f97d9
swapnilbarde007 <swapnilbarde007@gmail.com>
2025-10-28 18:40:55 +0530

    Initial commit 1

---
+++ src/main/java/com/telcomate/TelcoMate/TelcoMateApplication.java
@@ -0,0 +1,13 @@
+package com.telcomate.TelcoMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelcoMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelcoMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telcomate/TelcoMate/TelcoMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telcomate.TelcoMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelcoMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
